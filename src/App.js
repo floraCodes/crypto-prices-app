47c8b67e3d690e0e1e3d93690ee5bb4e4a95bfe1
flormcalvo <flor.calvo89@gmail.com>
2020-05-30 20:18:57 +1000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "@emotion/styled";
+import image from "./cryptomonedas.png";
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <div>
+        <Image src={image} alt="crypto image" />
+      </div>
+      <div>
+        <Heading>Live Crypto Prices</Heading>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
